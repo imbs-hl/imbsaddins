786a9358993a3a4c4678c5ba8102359195ab6585
Damian Gola <gola@imbs.uni-luebeck.de>
2017-03-30 17:13:41 +0200

    initial commit

---
+++ R/word_count.R
@@ -0,0 +1,28 @@
+#' Count words in selection
+#'
+#' @description Count the words in the selected text in the source editor.
+#'
+#' @return Number of words
+#'
+#' @family word_count
+#'
+#' @export
+selection_word_count <- function() {
+  selection <- rstudioapi::getSourceEditorContext()$selection[[1]]$text
+  word_count <- qdap::wc(selection)
+  message(sprintf("%d words in selection.", word_count))
+}
+
+#' Count words in active document
+#'
+#' @description Count the words in the active source editor document.
+#'
+#' @return Number of words
+#' @export
+#'
+#' @family word_count
+document_word_count <- function() {
+  content <- rstudioapi::getSourceEditorContext()$contents
+  word_count <- sum(qdap::wc(content), na.rm = TRUE)
+  message(sprintf("%d words in document.", word_count))
+}
